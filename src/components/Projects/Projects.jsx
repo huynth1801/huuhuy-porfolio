a7ca7487b40ff8d0126ba4e1961d57c75b15617d
huynth1801 <huuhuy1801@gmail.com>
2023-05-02 08:02:16 +0700

    clean projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,4 @@
-import theband from "../../assets/img/theband.png";
-import food from "../../assets/img/food.png";
-import nike from "../../assets/img/nike.png";
-import helendo from "../../assets/img/helendo.png";
+import data from "./data";
 import "./projects.css";
 import { motion } from "framer-motion";
 import { fadeIn } from "../fadeIn";
@@ -25,8 +22,9 @@ const Projects = () => {
         {/* projects */}
         <div className="my-12 mx-4 grid grid-cols-1 gap-10 z-10">
           {/* food */}
-          <div
-            className="recent__project-card project__slide-up my-4
+          {data.map((item, index) => (
+            <div
+              className="recent__project-card project__slide-up my-4
             flex 
             flex-col 
             text-center 
@@ -44,153 +42,45 @@ const Projects = () => {
             dark:bg-[#464749]
             dark:hover:bg-pink-800
             "
-          >
-            <img
-              src={food}
-              alt="Food order website"
-              className="rounded-lg w-full md:h-[22rem]"
-            />
-            <div className="flex flex-col justify-between dark:text-white">
-              <div className="px-5 pt-6 md:text-left">
-                <div className="text-xl font-semibold">Grab food</div>
-                <div className="text-zinc-500 pt-4 dark:text-white">
-                  A simple demo landing page website, i did this project to
-                  study TailwindCSS and responsive layout.
+              key={index}
+            >
+              <img
+                src={item.imgPath}
+                alt="Food order website"
+                className="rounded-lg w-full md:h-[22rem]"
+              />
+              <div className="flex flex-col justify-between dark:text-white">
+                <div className="px-5 pt-6 md:text-left">
+                  <div className="text-xl font-semibold">
+                    {item.projectName}
+                  </div>
+                  <div className="text-zinc-500 pt-4 dark:text-white">
+                    {item.prjDesc}
+                  </div>
+                </div>
+                <div className="flex flex-row justify-center items-center mt-8 md:justify-start ">
+                  <a
+                    href={item.githubLink}
+                    target="_blank"
+                    className="btn__link py-2 px-4 mx-4"
+                  >
+                    <button className="text-sm text-slate-600 hover dark:text-white">
+                      View source
+                    </button>
+                  </a>
+                  <a
+                    href={item.liveDemo}
+                    target="_blank"
+                    className="btn__link py-2 px-4"
+                  >
+                    <button className="text-sm text-slate-600 hover dark:text-white">
+                      Visit website
+                    </button>
+                  </a>
                 </div>
               </div>
-              <div className="flex flex-row justify-center items-center mt-8 md:justify-start ">
-                <a
-                  href="https://github.com/huynth1801/landing-page-tailwind"
-                  target="_blank"
-                  className="btn__link py-2 px-4 mx-4"
-                >
-                  <button className="text-sm text-slate-600 hover dark:text-white">
-                    View source
-                  </button>
-                </a>
-                <a
-                  href="https://comforting-daifuku-846607.netlify.app/"
-                  target="_blank"
-                  className="btn__link py-2 px-4"
-                >
-                  <button className="text-sm text-slate-600 hover dark:text-white">
-                    Visit website
-                  </button>
-                </a>
-              </div>
             </div>
-          </div>
-          {/* Nike */}
-          <div
-            className="recent__project-card project__slide-up my-4
-            flex 
-            flex-col 
-            text-center 
-            items-center 
-            justify-center
-            duration-300
-            cursor-pointer
-            hover:bg-fuchsia-100
-            md:hover:scale-105
-            md:grid
-            md:grid-cols-2
-            md:gap-10
-            md:mx-38
-            lg:mx-28
-            dark:bg-[#464749]
-            dark:hover:bg-pink-800
-            "
-          >
-            <img
-              src={nike}
-              alt="Nike clone website"
-              className="w-full rounded-t-lg md:h-[22rem]"
-            />
-            <div className="flex flex-col justify-between dark:text-white">
-              <div className="px-5 pt-6 md:text-left">
-                <div className="text-xl font-semibold">Nike</div>
-                <div className="text-zinc-500 pt-4 dark:text-white">
-                  Nike clone website, not responsive yet.
-                </div>
-              </div>
-              <div className="flex flex-row justify-center items-center mt-8 md:justify-start">
-                <a
-                  href="https://github.com/huynth1801/NikeWebsite"
-                  target="_blank"
-                  className="btn__link py-2 px-4 mx-4"
-                >
-                  <button className="text-sm text-slate-600 hover dark:text-white">
-                    View source
-                  </button>
-                </a>
-                <a
-                  href="https://huynth1801.github.io/NikeWebsite/"
-                  target="_blank"
-                  className="btn__link py-2 px-4"
-                >
-                  <button className="text-sm text-slate-600 hover dark:text-white">
-                    Visit website
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Helendo */}
-          <div
-            className="recent__project-card project__slide-up my-4
-            flex 
-            flex-col 
-            text-center 
-            items-center 
-            justify-center
-            duration-300
-            cursor-pointer
-            hover:bg-fuchsia-100
-            md:hover:scale-105
-            md:grid
-            md:grid-cols-2
-            md:gap-10
-            md:mx-38
-            lg:mx-28
-            dark:bg-[#464749]
-            dark:hover:bg-pink-800
-            "
-          >
-            <img
-              src={helendo}
-              alt="Food order website"
-              className="w-full rounded-t-lg md:h-[22rem]"
-            />
-            <div className="flex flex-col justify-between dark:text-white">
-              <div className="px-5 pt-3 md:text-left">
-                <div className="text-xl font-semibold">E-commerce Shop</div>
-                <div className="text-zinc-500 pt-4 dark:text-white">
-                  An online shop, i created to learn more about ReactJS, Redux,
-                  TailwindCSS
-                </div>
-              </div>
-              <div className="flex flex-row items-center justify-center mt-8 md:justify-start">
-                <a
-                  href="https://github.com/huynth1801/Shop-clone-react"
-                  target="_blank"
-                  className="btn__link py-2 px-4 mx-4"
-                >
-                  <button className="text-sm text-slate-600 hover dark:text-white">
-                    View source
-                  </button>
-                </a>
-                <a
-                  href="https://helendoshop.netlify.app/home"
-                  target="_blank"
-                  className="btn__link py-2 px-4"
-                >
-                  <button className="text-sm text-slate-600 hover dark:text-white">
-                    Visit website
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </motion.div>
     </section>
---
+++ src/components/Projects/data.js
@@ -0,0 +1,45 @@
+import theband from "../../assets/img/theband.png";
+import food from "../../assets/img/food.png";
+import nike from "../../assets/img/nike.png";
+import helendo from "../../assets/img/helendo.png";
+import reactMovie from "../../assets/img/react_movie.png";
+
+const data = [
+  {
+    id: "01",
+    projectName: "Grab food",
+    imgPath: food,
+    prjDesc:
+      "A simple demo landing page website, i did this project to study TailwindCSS and responsive layout.",
+    githubLink: "https://github.com/huynth1801/landing-page-tailwind",
+    liveDemo: "https://comforting-daifuku-846607.netlify.app/",
+  },
+  {
+    id: "02",
+    projectName: "Nike website",
+    imgPath: nike,
+    prjDesc: "Nike clone website, not responsive yet.",
+    githubLink: "https://github.com/huynth1801/NikeWebsite",
+    liveDemo: "https://huynth1801.github.io/NikeWebsite/",
+  },
+  {
+    id: "03",
+    projectName: "React Movie App",
+    imgPath: reactMovie,
+    prjDesc: "React Movie App with Dark Mode using ReactJS, TailwindCSS, SCSS",
+    githubLink:
+      "https://github.com/huynth1801/react-tailwind-movieapp/tree/master",
+    liveDemo: "https://sunfix.netlify.app/",
+  },
+  {
+    id: "04",
+    projectName: "E-commerce website",
+    imgPath: helendo,
+    prjDesc:
+      "An online shop, i created to learn more about ReactJS, Redux, TailwindCSS",
+    githubLink: "https://github.com/huynth1801/Shop-clone-react",
+    liveDemo: "https://helendoshop.netlify.app/home",
+  },
+];
+
+export default data;
